8be89491e750e6a65ea2a5f4364b31ad0b4ea3b2
tantality <angel.tretyak@mail.ru>
2023-03-02 00:50:57 +0300

    feat: add language module constants

---
+++ src/languages/languages.constants.ts
@@ -0,0 +1,10 @@
+export const MIN_CODE_LENGTH = 2;
+export const MAX_CODE_LENGTH = 4;
+
+export const MIN_NAME_LENGTH = 2;
+export const MAX_NAME_LENGTH = 50;
+
+export enum LANGUAGE_SORT_BY {
+  NAME = 'name',
+  DATE = 'date',
+}

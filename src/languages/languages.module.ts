70d2a99a14ddaadaa935bbb3421665cca2b8790a
tantality <angel.tretyak@mail.ru>
2023-03-08 20:10:58 +0300

    feat: define the language model

--- src/languages/languages.module.ts
+++ src/languages/languages.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Language, LanguageSchema } from './language.schema';
 import { LanguagesController } from './languages.controller';
 import { LanguagesRepository } from './languages.repository';
 import { LanguagesService } from './languages.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Language.name, schema: LanguageSchema }])],
   controllers: [LanguagesController],
   providers: [LanguagesService, LanguagesRepository],
+  exports: [LanguagesService, MongooseModule],
 })
 export class LanguagesModule {}
 

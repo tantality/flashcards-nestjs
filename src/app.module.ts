66ff68dbe2cfbaf7cdf544a90922a6704d1cc883
tantality <angel.tretyak@mail.ru>
2023-03-08 18:13:34 +0300

    feat: implement connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 /* eslint-disable require-await */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { EnvConfigModule } from './config/env-config/env-config.module';
 import { LanguagesModule } from './languages/languages.module';
 
 @Module({
-  imports: [EnvConfigModule, LanguagesModule],
+  imports: [EnvConfigModule, MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URI as string), LanguagesModule],
 })
 export class AppModule {}

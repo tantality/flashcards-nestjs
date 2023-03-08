417d749c4205f543f776dc0daa0e188b4c73a69c
tantality <angel.tretyak@mail.ru>
2023-03-08 19:50:45 +0300

    refactor: apply the app port from the config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './common/pipes';
+import { EnvConfigService } from './config/env-config/env-config.service';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3000);
+  const port = app.get(EnvConfigService).getAppPort();
+  await app.listen(port);
 }
 bootstrap();
 

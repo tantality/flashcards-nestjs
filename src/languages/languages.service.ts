bb61c969525693562126fdd1ddf01b6bb4364a05
tantality <angel.tretyak@mail.ru>
2023-03-15 00:33:55 +0300

    feat: set the service structure of the languages module

--- src/languages/languages.service.ts
+++ src/languages/languages.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ObjectId, FilterQuery } from 'mongoose';
+import { CreateLanguageDto, GetAllLanguagesQueryDto, UpdateLanguageDto } from './dto';
+import { Language } from './language.schema';
+import { LanguagesRepository } from './languages.repository';
 
 @Injectable()
-export class LanguagesService {}
+export class LanguagesService {
+  constructor(private languagesRepository: LanguagesRepository) {}
+
+  findAndCountAll = async (query: GetAllLanguagesQueryDto): Promise<{ count: number; languages: Language[] }> => {
+    const languagesAndTheirCount = await this.languagesRepository.findAndCountAll(query);
+    return languagesAndTheirCount;
+  };
+
+  create = async (createLanguageDto: CreateLanguageDto): Promise<Language> => {
+    const createLanguage = await this.languagesRepository.create(createLanguageDto);
+    return createLanguage;
+  };
+
+  update = async (id: ObjectId, updateLanguageDto: UpdateLanguageDto): Promise<Language> => {
+    const updatedLanguage = await this.languagesRepository.update(id, updateLanguageDto);
+    return updatedLanguage;
+  };
+
+  delete = async (id: ObjectId): Promise<void> => {
+    await this.languagesRepository.delete(id);
+  };
+
+  findOne = async (condition: FilterQuery<Language>): Promise<Language | null> => {
+    const language = await this.languagesRepository.findOne(condition);
+    return language;
+  };
+}
+

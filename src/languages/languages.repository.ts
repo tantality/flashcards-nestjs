84b698d9de4f13f8a534aa0e1480915c625985ff
tantality <angel.tretyak@mail.ru>
2023-03-15 00:07:29 +0300

    feat: set the repository structure of the languages module

--- src/languages/languages.repository.ts
+++ src/languages/languages.repository.ts
@@ -1,4 +1,39 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable require-await */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ObjectId, FilterQuery, Model } from 'mongoose';
+import { CreateLanguageDto, GetAllLanguagesQueryDto, UpdateLanguageDto } from './dto';
+import { Language, LanguageDocument } from './language.schema';
 
 @Injectable()
-export class LanguagesRepository {}
+export class LanguagesRepository {
+  constructor(@InjectModel(Language.name) private languageModel: Model<LanguageDocument>) {}
+
+  mockLanguage: Language = {
+    _id: '21' as unknown as ObjectId,
+    code: 'eng',
+    name: 'english',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  } as Language;
+
+  findAndCountAll = async (query: GetAllLanguagesQueryDto): Promise<{ count: number; languages: Language[] }> => {
+    return { count: 2, languages: [this.mockLanguage] };
+  };
+
+  create = async (createLanguageDto: CreateLanguageDto): Promise<Language> => {
+    return this.mockLanguage;
+  };
+
+  update = async (id: ObjectId, updateLanguageDto: UpdateLanguageDto): Promise<Language> => {
+    return this.mockLanguage;
+  };
+
+  findOne = async (condition: FilterQuery<Language>): Promise<Language | null> => {
+    return this.mockLanguage;
+  };
+
+  delete = async (id: ObjectId): Promise<void> => {};
+}

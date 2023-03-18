94e95873a1cc83563d9d0aaefad951a33c7cf8ad
tantality <angel.tretyak@mail.ru>
2023-03-18 16:17:15 +0300

    feat: implement the return of values from controller methods

--- src/languages/languages.controller.ts
+++ src/languages/languages.controller.ts
@@ -1,25 +1,47 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable require-await */
 import { Controller } from '@nestjs/common';
-import { Body, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common/decorators';
+import { Body, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common/decorators';
 import { ObjectId } from 'mongoose';
+import { SerializerInterceptor } from 'src/common/interceptors';
 import { ParseObjectIdPipe } from '../common/pipes';
-import { CreateLanguageDto, GetAllLanguagesQueryDto, UpdateLanguageDto } from './dto';
+import { CreateLanguageDto, GetAllLanguagesQueryDto, LanguageResponseDto, UpdateLanguageDto } from './dto';
+import { Language } from './language.schema';
+import { LanguagesService } from './languages.service';
 
 @Controller('languages')
 export class LanguagesController {
+  constructor(private languagesService: LanguagesService) {}
+
   @Get()
-  async getAllLanguages(@Query() query: GetAllLanguagesQueryDto): Promise<void> {}
+  async getAllLanguages(@Query() query: GetAllLanguagesQueryDto): Promise<{ count: number; languages: Language[] }> {
+    const languagesAndTheirCount = await this.languagesService.findAndCountAll(query);
+    return languagesAndTheirCount;
+  }
 
   @Get(':id')
-  async getLanguage(@Param('id', ParseObjectIdPipe) id: ObjectId): Promise<void> {}
+  @UseInterceptors(new SerializerInterceptor(LanguageResponseDto))
+  async getLanguage(@Param('id', ParseObjectIdPipe) id: ObjectId): Promise<LanguageResponseDto> {
+    const language = (await this.languagesService.findOne(id)) as Language;
+    return language;
+  }
 
   @Post()
-  async createLanguage(@Body() createLanguageDto: CreateLanguageDto): Promise<void> {}
+  @UseInterceptors(new SerializerInterceptor(LanguageResponseDto))
+  async createLanguage(@Body() createLanguageDto: CreateLanguageDto): Promise<LanguageResponseDto> {
+    const createdLanguage = await this.languagesService.create(createLanguageDto);
+    return createdLanguage;
+  }
 
   @Patch(':id')
-  async updateLanguage(@Param('id', ParseObjectIdPipe) id: ObjectId, @Body() updateLanguageDto: UpdateLanguageDto): Promise<void> {}
+  @UseInterceptors(new SerializerInterceptor(LanguageResponseDto))
+  async updateLanguage(
+    @Param('id', ParseObjectIdPipe) id: ObjectId,
+      @Body() updateLanguageDto: UpdateLanguageDto,
+  ): Promise<LanguageResponseDto> {
+    const updatedLanguage = await this.languagesService.update(id, updateLanguageDto);
+    return updatedLanguage;
+  }
 
   @Delete(':id')
   async deleteLanguage(@Param('id', ParseObjectIdPipe) id: ObjectId): Promise<void> {}

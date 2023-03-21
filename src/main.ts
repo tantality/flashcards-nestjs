3c827f8b5292e1069af729d57d2b4d54f17e4e30
tantality <angel.tretyak@mail.ru>
2023-03-21 20:09:05 +0300

    feat: connect swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './common/pipes';
 import { EnvConfigService } from './config/env-config/env-config.service';
@@ -8,8 +9,15 @@ async function bootstrap(): Promise<void> {
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  const config = new DocumentBuilder()
+    .setTitle('Flashcards')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
   const port = app.get(EnvConfigService).getAppPort();
   await app.listen(port);
 }
 bootstrap();
-

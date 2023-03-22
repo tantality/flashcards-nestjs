6afa0f2e028ce0116fadde52180831ba5c0ef4cf
tantality <angel.tretyak@mail.ru>
2023-03-22 19:11:19 +0300

    docs: add summaries for controller handlers

--- src/languages/languages.controller.ts
+++ src/languages/languages.controller.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Controller, NotFoundException } from '@nestjs/common';
 import { Body, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common/decorators';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ObjectId } from 'mongoose';
 import { LanguageExceptionMessages } from 'src/common/constants';
 import { SerializerInterceptor } from 'src/common/interceptors';
@@ -10,17 +9,20 @@ import { CreateLanguageDto, GetAllLanguagesQueryDto, LanguageResponseDto, Update
 import { Language } from './language.schema';
 import { LanguagesService } from './languages.service';
 
+@ApiTags('languages')
 @Controller('languages')
 export class LanguagesController {
   constructor(private languagesService: LanguagesService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get a list of languages' })
   async getAllLanguages(@Query() query: GetAllLanguagesQueryDto): Promise<{ count: number; languages: Language[] }> {
     const languagesAndTheirCount = await this.languagesService.findAndCountAll(query);
     return languagesAndTheirCount;
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a language' })
   @UseInterceptors(new SerializerInterceptor(LanguageResponseDto))
   async getLanguage(@Param('id', ParseObjectIdPipe) id: ObjectId): Promise<LanguageResponseDto> {
     const language = await this.languagesService.findOne({ _id: id });
@@ -32,6 +34,7 @@ export class LanguagesController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a language' })
   @UseInterceptors(new SerializerInterceptor(LanguageResponseDto))
   async createLanguage(@Body() createLanguageDto: CreateLanguageDto): Promise<LanguageResponseDto> {
     const createdLanguage = await this.languagesService.create(createLanguageDto);
@@ -39,6 +42,7 @@ export class LanguagesController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update an existing language' })
   @UseInterceptors(new SerializerInterceptor(LanguageResponseDto))
   async updateLanguage(
     @Param('id', ParseObjectIdPipe) id: ObjectId,
@@ -49,6 +53,7 @@ export class LanguagesController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a language' })
   async deleteLanguage(@Param('id', ParseObjectIdPipe) id: ObjectId): Promise<void> {
     await this.languagesService.delete(id);
   }

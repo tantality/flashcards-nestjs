da700a9db2425a358f47837b123eb78742b2deb8
tantality <angel.tretyak@mail.ru>
2023-03-29 01:58:54 +0300

    feat: update the user response dto

--- src/modules/users/dto/response.dto.ts
+++ src/modules/users/dto/response.dto.ts
@@ -1,8 +1,27 @@
+import { Exclude, Expose, instanceToPlain, Transform } from 'class-transformer';
 import { ObjectId } from 'mongoose';
 
+@Exclude()
 export class UserResponseDto {
+  @Expose({ name: 'id' })
+  @Transform(({ obj }) => obj._id.toString())
   readonly _id: ObjectId;
+
+  @Expose()
   readonly name: string;
+
+  @Expose()
   readonly email: string;
+
+  @Expose()
+  @Transform(({ obj }) => obj._id.toString())
   readonly nativeLanguageId: ObjectId;
+
+  constructor(partial: Partial<UserResponseDto>) {
+    Object.assign(this, partial);
+  }
+
+  toJSON?(): Record<string, any> {
+    return instanceToPlain(this);
+  }
 }

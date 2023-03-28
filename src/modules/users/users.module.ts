2d021bc2a69bbb399fd477e88dc80ee94287d280
tantality <angel.tretyak@mail.ru>
2023-03-29 01:24:23 +0300

    feat: implement the service methods of the users module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LanguagesModule } from '../languages/languages.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { User, UserSchema } from './user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), LanguagesModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
-
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { FilterQuery, ObjectId } from 'mongoose';
+import { LANGUAGE_EXCEPTION_MESSAGES, USER_EXCEPTION_MESSAGES } from 'src/common/constants';
+import { LanguagesService } from '../languages/languages.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './user.schema';
@@ -9,18 +9,37 @@ import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private userRepository: UsersRepository) {}
+  constructor(private usersRepository: UsersRepository, private languagesServices: LanguagesService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.userRepository.create(createUserDto);
-  }
+  create = async (createUserDto: CreateUserDto): Promise<User> => {
+    const nativeLanguage = await this.languagesServices.findOne({ _id: createUserDto.nativeLanguageId });
+    if (!nativeLanguage) {
+      throw new NotFoundException(LANGUAGE_EXCEPTION_MESSAGES.NOT_FOUND);
+    }
 
-  update(id: ObjectId, updateUserDto: UpdateUserDto) {
-    return this.userRepository.update(id, updateUserDto);
-  }
+    const user = await this.findOne({ normalizedEmail: createUserDto.normalizedEmail });
+    if (user) {
+      throw new BadRequestException(USER_EXCEPTION_MESSAGES.ALREADY_EXISTS);
+    }
 
-  findOne(condition: FilterQuery<User>) {
-    return this.userRepository.findOne(condition);
-  }
+    const createdUser = await this.usersRepository.create(createUserDto);
+
+    return createdUser;
+  };
+
+  update = async (id: ObjectId, updateUserDto: UpdateUserDto): Promise<User> => {
+    const nativeLanguage = await this.languagesServices.findOne({ _id: updateUserDto.nativeLanguageId });
+    if (!nativeLanguage) {
+      throw new NotFoundException(LANGUAGE_EXCEPTION_MESSAGES.NOT_FOUND);
+    }
+
+    const updatedUser = await this.usersRepository.update(id, updateUserDto);
+
+    return updatedUser;
+  };
+
+  findOne = async (condition: FilterQuery<User>): Promise<User | null> => {
+    const user = await this.usersRepository.findOne(condition);
+    return user;
+  };
 }
-

9676ab80bff1707a468b81ddefcf942d2c458158
tantality <angel.tretyak@mail.ru>
2023-04-05 16:22:29 +0300

    feat: add dto validation

--- src/modules/auth/dto/log-in.dto.ts
+++ src/modules/auth/dto/log-in.dto.ts
@@ -1,4 +1,18 @@
+import { IsEmail, Matches } from 'class-validator';
+import { RemoveExtraSpaces } from 'src/common/decorators';
+import { MIN_PASSWORD_LENGTH } from '../auth.constants';
+
+const STRONG_PASSWORD_REG_EXP = new RegExp(
+  '(?=.*[0-9])(?=.*[!@#$%^&*()_+=])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()_+=]{' + MIN_PASSWORD_LENGTH + ',}',
+  'g',
+);
+
 export class LogInDto {
+  @Matches(STRONG_PASSWORD_REG_EXP)
+  @RemoveExtraSpaces()
   readonly password: string;
+
+  @IsEmail()
+  @RemoveExtraSpaces()
   readonly email: string;
 }
--- src/modules/auth/dto/sign-up.dto.ts
+++ src/modules/auth/dto/sign-up.dto.ts
@@ -1,7 +1,15 @@
+import { IsMongoId, IsString, Length } from 'class-validator';
 import { ObjectId } from 'mongoose';
+import { RemoveExtraSpaces } from 'src/common/decorators';
+import { MIN_NAME_LENGTH, MAX_NAME_LENGTH } from '../auth.constants';
 import { LogInDto } from './log-in.dto';
 
 export class SignUpDto extends LogInDto {
+  @Length(MIN_NAME_LENGTH, MAX_NAME_LENGTH)
+  @RemoveExtraSpaces()
+  @IsString()
   readonly name: string;
+
+  @IsMongoId()
   readonly nativeLanguageId: ObjectId;
 }

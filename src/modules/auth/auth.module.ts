f3f81139331657c5d2019d1385059ea6b0b9bdfb
tantality <angel.tretyak@mail.ru>
2023-04-09 17:36:46 +0300

    feat: register the access token guard as a global

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt/dist';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EnvConfigModule } from 'src/config/env-config/env-config.module';
 import { User, UserSchema } from '../users/user.schema';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
+import { AccessTokenAuthGuard } from './guards';
 import { RefreshTokenRepository } from './repositories/refresh-token.repository';
 import { AuthService } from './services/auth.service';
 import { JWTService } from './services/jwt.service';
@@ -23,6 +25,15 @@ import { AccessTokenStrategy } from './strategies';
     ]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JWTService, RefreshTokenRepository, AccessTokenStrategy],
+  providers: [
+    AuthService,
+    JWTService,
+    RefreshTokenRepository,
+    AccessTokenStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenAuthGuard,
+    },
+  ],
 })
 export class AuthModule {}

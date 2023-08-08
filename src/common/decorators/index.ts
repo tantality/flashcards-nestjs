e262c342e16192249bf5db6c4db486ca25f48787
tantality <angel.tretyak@mail.ru>
2023-08-08 22:13:23 +0300

    feat: add several decorators

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,3 +1,5 @@
 export * from './to-lower-case.decorator';
 export * from './remove-extra-spaces.decorator';
 export * from './objectId-to-string.decorator';
+export * from './user.decorator';
+export * from './refreshToken.decorator';
---
+++ src/common/decorators/refreshToken.decorator.ts
@@ -0,0 +1,6 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const RefreshToken = createParamDecorator((data: unknown, context: ExecutionContext) => {
+  const request = context.switchToHttp().getRequest();
+  return request.cookies['refreshToken'];
+});

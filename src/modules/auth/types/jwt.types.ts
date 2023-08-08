de037a205ccb0c611b3257d968e50939dad8dbb7
tantality <angel.tretyak@mail.ru>
2023-08-08 21:27:46 +0300

    feat: add types

--- src/modules/auth/types/jwt.types.ts
+++ src/modules/auth/types/jwt.types.ts
@@ -1,12 +1,25 @@
 import { ObjectId } from 'mongoose';
 import { USER_ROLE } from 'src/modules/users/users.constants';
 
-export interface JwtPayload {
+interface JwtPayload {
+  iat: number;
+  exp: number;
+}
+
+export interface UserJwtPayload {
   userId: ObjectId;
   role: USER_ROLE;
 }
 
+export type DecodedUserJwtPayload = JwtPayload & UserJwtPayload;
+
 export interface JwtTokens {
   accessToken: string;
   refreshToken: string;
+}
+
+export interface RefreshToken {
+  _id: ObjectId;
+  value: string;
+  expiresAt: Date;
 }

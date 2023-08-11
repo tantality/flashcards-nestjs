70d9444f0880b07ea67bbd63f9ecf05a4cee166d
tantality <angel.tretyak@mail.ru>
2023-08-11 18:53:54 +0300

    feat: adjust work with incoming data in the user module

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Body, Patch, NotFoundException, UseInterceptors } from '@nestjs/common';
-import { ObjectId } from 'mongoose';
 import { ApiBadRequestResponse, ApiBearerAuth, ApiNotFoundResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RESPONSE_STATUS_DESCRIPTION, USER_EXCEPTION_MESSAGE } from 'src/common/constants';
 import { SerializerInterceptor } from 'src/common/interceptors';
+import { User } from 'src/common/decorators';
+import { DecodedUserJwtPayload } from '../auth/types';
 import { UsersService } from './users.service';
 import { UpdateUserDto, UserResponseDto } from './dto';
 
@@ -10,19 +11,16 @@ import { UpdateUserDto, UserResponseDto } from './dto';
 @ApiBearerAuth()
 @Controller('users')
 export class UsersController {
-  MOCK_USER_ID = '63a21c308ac9d6783208b8d7' as unknown as ObjectId;
-
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
   @ApiOperation({ summary: 'Get a user' })
   @UseInterceptors(new SerializerInterceptor(UserResponseDto))
-  async getUser(): Promise<UserResponseDto> {
-    const user = await this.usersService.findOne({ id: this.MOCK_USER_ID });
+  async getUser(@User() payload: DecodedUserJwtPayload): Promise<UserResponseDto> {
+    const user = await this.usersService.findOne({ _id: payload.userId });
     if (!user) {
       throw new NotFoundException(USER_EXCEPTION_MESSAGE.NOT_FOUND);
     }
-
     return user;
   }
 
@@ -31,8 +29,8 @@ export class UsersController {
   @ApiBadRequestResponse({ description: RESPONSE_STATUS_DESCRIPTION.BAD_REQUEST })
   @ApiNotFoundResponse({ description: RESPONSE_STATUS_DESCRIPTION.NOT_FOUND })
   @UseInterceptors(new SerializerInterceptor(UserResponseDto))
-  async updateUser(@Body() updateUserDto: UpdateUserDto): Promise<UserResponseDto> {
-    const updatedLanguage = await this.usersService.update(this.MOCK_USER_ID, updateUserDto);
+  async updateUser(@User() payload: DecodedUserJwtPayload, @Body() updateUserDto: UpdateUserDto): Promise<UserResponseDto> {
+    const updatedLanguage = await this.usersService.update(payload.userId, updateUserDto);
     return updatedLanguage;
   }
 }

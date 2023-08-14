4fbe98734e8d9ff6ee9917ec2337d5041ed33019
tantality <angel.tretyak@mail.ru>
2023-08-14 17:24:40 +0300

    feat: implement the creation of conditions for searching for cards

--- src/modules/cards/cards.repository.ts
+++ src/modules/cards/cards.repository.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId, SchemaTypes } from 'mongoose';
+import { FilterQuery, Model, ObjectId, SchemaTypes } from 'mongoose';
 import { Card, CardDocument } from './card.schema';
 import { GetAllCardsQueryDto, AllCardsResponseDto } from './dto';
 
@@ -27,6 +27,38 @@ export class CardsRepository {
   } as AllCardsResponseDto;
 
   findAndCountAll = async (userId: ObjectId, query: GetAllCardsQueryDto): Promise<AllCardsResponseDto> => {
+    const { search, sortBy, sortDirection, limit, offset, languageId } = query;
+
+    const findingCondition = this.createFindingConditionForCards({ userId, languageId, search });
+
     return this.CARDS_SET;
+  };
+
+  private createFindingConditionForCards = (
+    conditionParameters: Pick<GetAllCardsQueryDto, 'languageId' | 'search'> & { userId: ObjectId },
+  ): FilterQuery<Card> => {
+    const { search, languageId, userId } = conditionParameters;
+
+    const searchInWordsCondition = this.createSearchInWordsCondition(search);
+    const languagesCondition = this.createLanguagesCondition(languageId);
+
+    const condition: FilterQuery<Card> = {
+      userId,
+      $and: [searchInWordsCondition, languagesCondition],
+    };
+
+    return condition;
+  };
+
+  private createSearchInWordsCondition = (search?: string): FilterQuery<Card> => {
+    const searchCondition = search ? { $regex: new RegExp(search, 'i') } : null;
+    const searchInWordsCondition = searchCondition ? { $or: [{ nativeWords: searchCondition }, { foreignWords: searchCondition }] } : {};
+
+    return searchInWordsCondition;
+  };
+
+  private createLanguagesCondition = (languageId?: ObjectId): FilterQuery<Card> => {
+    const languagesCondition = languageId ? { $or: [{ nativeLanguageId: languageId }, { foreignLanguageId: languageId }] } : {};
+    return languagesCondition;
   };
 }

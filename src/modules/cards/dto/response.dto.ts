b19d13a6cf043d8cad80ff9a669894b82cc04771
tantality <angel.tretyak@mail.ru>
2023-08-14 16:11:16 +0300

    feat: implement CardResponseDto

--- src/modules/cards/dto/response.dto.ts
+++ src/modules/cards/dto/response.dto.ts
@@ -1,7 +1,38 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose, instanceToPlain } from 'class-transformer';
+import { ObjectId } from 'mongoose';
+import { ObjectIdToString } from 'src/common/decorators';
 
 @Exclude()
-export class CardResponseDto {}
+export class CardResponseDto {
+  @Expose({ name: 'id' })
+  @ObjectIdToString()
+  readonly _id: ObjectId;
+
+  @Expose()
+  @ObjectIdToString()
+  readonly nativeLanguageId: ObjectId;
+
+  @Expose()
+  readonly nativeWords: string[];
+
+  @Expose()
+  @ObjectIdToString()
+  readonly foreignLanguageId: ObjectId;
+
+  @Expose()
+  readonly foreignWords: string[];
+
+  @Expose()
+  readonly createdAt: Date;
+
+  constructor(partial: Partial<CardResponseDto>) {
+    Object.assign(this, partial);
+  }
+
+  toJSON?(): Record<string, any> {
+    return instanceToPlain(this);
+  }
+}
 
 @Exclude()
 export class AllCardsResponseDto {}
